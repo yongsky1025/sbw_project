a67d115e253e967df24f946b9a6b819033175f6d
yongsky1025 <qaz7984@gmail.com>
2026-05-12 15:08:32 +0900

    backend commit

---
+++ sbw_back/src/main/java/com/soldesk/sbw_back/SbwBackApplication.java
@@ -0,0 +1,13 @@
+package com.soldesk.sbw_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbwBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbwBackApplication.class, args);
+	}
+
+}
---
+++ sbw_back/src/test/java/com/soldesk/sbw_back/SbwBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soldesk.sbw_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
